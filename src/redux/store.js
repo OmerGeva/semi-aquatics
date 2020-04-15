e9ff6ec75fc8d65b9ab509e84388f673333ce6d3
Omer Geva <ogeva97@gmail.com>
2020-04-15 15:40:14 +0300

    checking logger for heroku:

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,11 +5,11 @@ import { persistStore } from 'redux-persist'
 
 import rootReducer from './root-reducer';
 
-const middlewares = [thunk];
+const middlewares = [thunk, logger];
 
-if(process.env.NODE_ENV === 'development') {
-  middlewares.push(logger);
-}
+// if(process.env.NODE_ENV === 'development') {
+//   middlewares.push(logger);
+// }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
