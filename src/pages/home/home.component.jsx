635ddb5a961f8712e95361bbb58145cb62dfff22
Omer Geva <ogeva97@gmail.com>
2020-04-16 18:22:02 +0300

    bug fix for mobile navbar

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -6,6 +6,8 @@ const Home = () => (
   <div className="home-logo">
     <h2>[Reverse Evolution]</h2>
     <img src="https://cdn.shopify.com/s/files/1/0276/3305/7867/files/big3_1410x.png?v=1575617303" alt="logo"/>
+
+    <h4>SUBSCRIBE</h4>
   </div>
   )
 
