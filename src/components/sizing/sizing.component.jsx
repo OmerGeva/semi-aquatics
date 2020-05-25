6ba664a4bfe054d9b6f8f886d7a55822e19e5600
Omer Geva <ogeva97@gmail.com>
2020-05-25 18:37:16 +0300

    add sweatpants sizing chart

--- src/components/sizing/sizing.component.jsx
+++ src/components/sizing/sizing.component.jsx
@@ -14,6 +14,8 @@ export const Sizing = () => (
     <img src="https://cdn.shopify.com/s/files/1/0276/3305/7867/files/Screen_Shot_2020-01-27_at_6.51.02_PM_1944x.png?v=1580169073" alt="tshirt-sizing-chart"/>
     <h5>BOXY TEE</h5>
     <img src={boxyTee} alt="boxy-tshirt-sizing-chart"/>
+    <h5>SWEATPANTS</h5>
+    <img src="https://i.shgcdn.com/c7b99861-c6ce-49d0-bc8b-b884a973c6c9/-/format/auto/-/preview/3000x3000/-/quality/lighter/" alt="tshirt-sizing-chart"/>
   </SizingContainer>
   )
 
