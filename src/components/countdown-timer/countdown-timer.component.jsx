f53c85028c2aaf48a60628837a8903545a2cf15e
Omer Geva <ogeva97@gmail.com>
2020-06-22 02:01:09 +0300

    timer fix

--- src/components/countdown-timer/countdown-timer.component.jsx
+++ src/components/countdown-timer/countdown-timer.component.jsx
@@ -3,10 +3,11 @@ import { CountdownTimerContainer } from './countdown-timer.styles'
 
 const CountdownTimer = ({ timeLeft, timerComponents }) =>
 {
-  const days = timerComponents[0] ? timerComponents[0].props.children[0] : '0';
-  const hours = timerComponents[1] ? timerComponents[1].props.children[0] : '0';
-  const minutes = timerComponents[2] ? timerComponents[2].props.children[0] : '0';
-  const seconds = timerComponents[3] ? timerComponents[3].props.children[0] : '0';
+  let days = timeLeft.days;
+  let hours = timeLeft.hours;
+  let minutes = timeLeft.minutes;
+  let seconds = timeLeft.seconds;
+
   return(
     <CountdownTimerContainer>
         <div className="time-container">
