118b6419c57e9d8660cea7cc0d4479c34b625e42
Omer Geva <ogeva97@gmail.com>
2020-07-18 21:51:00 +0300

    banner change

--- src/components/announcement-banner/announcement-banner.component.jsx
+++ src/components/announcement-banner/announcement-banner.component.jsx
@@ -4,7 +4,7 @@ import { AnnouncementBannerContainer } from './announcement-banner.styles'
 const AnnouncementBanner = () => (
   <AnnouncementBannerContainer>
     <div className="photobanner">
-      <h5 className='slide-right'>SPRING/SUMER DROP 4 IS ON JULY 20TH AT 6:00 pm EST.</h5>
+      <h5 className='slide-right'>SPRING/SUMER DROP 4 IS ON JULY 20TH AT 6:00 pm EST. ALL ORDERS SHIP OUT IN 1-2 BUSINESS DAYS</h5>
     </div>
   </AnnouncementBannerContainer>
   )
