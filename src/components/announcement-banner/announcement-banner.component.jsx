c71a3c5b183c018c23d02e51d70ff7ed462fa823
Omer Geva <ogeva97@gmail.com>
2020-11-11 10:02:49 +0200

    banner text

--- src/components/announcement-banner/announcement-banner.component.jsx
+++ src/components/announcement-banner/announcement-banner.component.jsx
@@ -4,7 +4,7 @@ import { AnnouncementBannerContainer } from './announcement-banner.styles'
 const AnnouncementBanner = () => (
   <AnnouncementBannerContainer>
     <div className="photobanner">
-      <h5 className='slide-right'>Fall/Winter 2020/21 Drop 2 will be available on Monday, November 9th at 6:00 PM EST</h5>
+      <h5 className='slide-right'>CURRENTLY ALL SOLD OUT. ADD YOUR EMAIL TO FIND OUT WHEN OUR NEXT DROP IS. &nbsp; &nbsp; ALL ORDERS SHIP OUT IN 1-2 BUSINESS DAYS.</h5>
     </div>
   </AnnouncementBannerContainer>
   )
