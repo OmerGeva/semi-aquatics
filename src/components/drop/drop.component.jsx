f7dfc01d16e7046a2f017874daceaa5995692847
Omer Geva <ogeva97@gmail.com>
2021-06-26 22:52:08 +0300

    current drop

--- src/components/drop/drop.component.jsx
+++ src/components/drop/drop.component.jsx
@@ -53,7 +53,7 @@ const Drop = ({ match }) => {
       <div className="drop-page">
         <div className="drop-title">
           <h2>{currentDropHeader(parseInt(match.params.dropId))}</h2>
-          <h4>{currentDrop[0] ? currentDrop[0].description : ''}</h4>
+          {/* <h4>{currentDrop[0]  currentDrop[0].description : ''}</h4> */}
           <h5>{match.params.dropId === '7' &&
             "Happy Birthday, Will! 100% of profits from this drop will be donated to the William G. Nash Memorial Fund."
           }</h5>
