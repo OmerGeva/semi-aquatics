fc2f2f80f7c8b37f4198b42387f8d353b508d334
Omer Geva <ogeva97@gmail.com>
2021-09-07 09:43:39 +0300

    sold out details

--- src/components/announcement-banner/announcement-banner.component.jsx
+++ src/components/announcement-banner/announcement-banner.component.jsx
@@ -4,7 +4,7 @@ import { AnnouncementBannerContainer } from './announcement-banner.styles'
 const AnnouncementBanner = () => (
   <AnnouncementBannerContainer>
     <div className="photobanner">
-      <h5 className='slide-right'>Spring/Summer Drop 5 will be available on Monday, September 6th at 6:00 PM EST.&nbsp; &nbsp;&nbsp;&nbsp; &nbsp; ALL ORDERS SHIP OUT IN 1-2 DAYS.</h5>
+      <h5 className='slide-right'>CURRENTLY ALL SOLD OUT. ADD YOUR EMAIL TO FIND OUT WHEN OUR NEXT DROP IS&nbsp; &nbsp;&nbsp;&nbsp; &nbsp; ALL ORDERS SHIP OUT IN 1-2 DAYS.</h5>
     </div>
   </AnnouncementBannerContainer>
   )
